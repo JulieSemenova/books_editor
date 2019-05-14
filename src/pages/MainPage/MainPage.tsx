f23ee6fbf02dc34c131d45a5fda2c6a0cde3e83a
JulieSemenova <julie.loggi@gmail.com>
2019-05-14 20:57:35 +0300

    ✨simple project structure

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MainPage.css';
+
+const MainPage: React.FC = () => {
+  return (
+    <div>
+      <span>MainPage</span>
+    </div>
+  );
+}
+
+export default MainPage;
