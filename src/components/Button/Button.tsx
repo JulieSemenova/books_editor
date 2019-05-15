fa7008eda7215947e057aba74a6ccd204746aa87
Julie Semenova <js@id-media.ru>
2019-05-15 10:36:34 +0300

    ✨ add button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import './Button.css';
+
+interface Props {
+  className?: string;
+  title?: string;
+  children?: React.ReactNode;
+  onClick: (e?: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
+  tooltip?: string;
+}
+
+const Button: React.StatelessComponent<Props> = ({
+  onClick,
+  children,
+  title,
+  className,
+  disabled,
+  tooltip,
+}: Props): JSX.Element => {
+  const combinedClassName = ['button'];
+
+  if (className) {
+    combinedClassName.push(className);
+  }
+
+  return (
+    <>
+      <button
+        className={combinedClassName.join(' ')}
+        onClick={!disabled ? onClick : () => {}}
+        disabled={disabled}
+      >
+        {children || title || null}
+      </button>
+    </>
+  );
+};
+
+export default Button;
