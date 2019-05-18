a2a11ff1eebdf8a1c2e713fe81a7260b2df4754c
JulieSemenova <julie.loggi@gmail.com>
2019-05-18 21:42:59 +0300

    ✨filters for book list

--- src/components/BookList/BookList.tsx
+++ src/components/BookList/BookList.tsx
@@ -2,20 +2,58 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import BookCard from '../BookCard/BookCard';
-import { Books, ReduxState } from '../../types';
+import { Books, ReduxState, Filters } from '../../types';
 import './BookList.css';
 
 interface Props {
   bookList: Books.Book[];
+  param: Filters.SortParam;
+  direction: Filters.Diretion;
 }
 
 class BookList extends Component<Props, {}> {
+  prepareData = () => {
+    const { param, direction, bookList } = this.props;
+    if (param === 'title') {
+      const sortedBookList = bookList.sort((a: Books.Book, b: Books.Book) => {
+        const aString = a.title.toLowerCase();
+        const bString = b.title.toLowerCase();
+
+        if (aString > bString) {
+          return 1;
+        }
+        if (aString < bString) {
+          return -1;
+        }
+        return 0;
+      });
+      return direction === 'ASC' ? sortedBookList : sortedBookList.reverse();
+    }
+
+    if (param === 'year') {
+      const sortedBookList = bookList.sort((a: Books.Book, b: Books.Book) => {
+        const aValue = a.publicationYear ? +a.publicationYear : 0;
+        const bValue = b.publicationYear ? +b.publicationYear : 0;
+        if (aValue > bValue) {
+          return -1;
+        }
+        if (aValue < bValue) {
+          return 1;
+        }
+        return 0;
+      });
+      return direction === 'ASC' ? sortedBookList : sortedBookList.reverse();
+    }
+
+    return bookList;
+  };
+
   render() {
     const { bookList } = this.props;
     return (
       <div className="bookList">
         {bookList && bookList.length > 0 ? (
-          bookList.map((book: Books.Book) => {
+          this.prepareData().map((book: Books.Book) => {
             const { id, title, authors, pages } = book;
             return (
               <BookCard key={id} id={id} title={title} authors={authors} pages={pages} />
@@ -29,4 +67,8 @@ class BookList extends Component<Props, {}> {
   }
 }
 
-export default connect(({ books }: ReduxState) => ({ bookList: books.books }))(BookList);
+export default connect(({ books, filters }: ReduxState) => ({
+  bookList: books.books,
+  param: filters.param,
+  direction: filters.direction,
+}))(BookList);
